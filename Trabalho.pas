d542caad036c7588c677598c606c7885fd26f139
VictorRMarcelino <httpsvictorramos@gmail.com>
2025-03-13 21:25:30 -0300

    init

---
+++ Trabalho.pas
@@ -0,0 +1,30 @@
+Program Trabalho;
+
+const MAX_ARQUIBANCADA = 2000;
+const MAX_GERAL = 1000;
+const MAX_VISITANTES = 300;
+
+procedure iniciaMenu();
+var opcao: integer;
+begin;
+	while(opcao <> 5) do
+		begin;
+			writeln ('    MENU    ');
+			writeln ('------------');
+			writeln;
+			writeln (' 1 - Comprar Ingresso (Sócio)');
+			writeln (' 2 - Comprar Ingresso (Torcedor)');
+			writeln (' 3 - Comprar Ingresso (Visitante)');
+			writeln (' 4 - Comprar Ingresso (Sócio)');
+			writeln (' 5 - Sair');
+			writeln;
+			readln(opcao);
+			
+			
+			
+		end;
+end;
+
+Begin
+  
+End.
